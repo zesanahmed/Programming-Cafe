a548914ea35f320c04fa694289c4b134423e4985
zesanahmed <zesanahmed593@gmail.com>
2023-05-10 00:25:05 +0600

    tailwind css added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <button className="btn btn-secondary">Button</button>
+            <button className="btn btn-accent">Button</button>
+            <button className="btn btn-ghost">Button</button>
+        </div>
+    );
+};
+
+export default Header;

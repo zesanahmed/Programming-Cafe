dba657ea2f6fdc973fd11d8ed48fc249d22dbb1e
zesanahmed <zesanahmed593@gmail.com>
2023-05-10 22:23:42 +0600

    blog dataloaded

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => {
-    console.log(props)
+    console.log(props.blog)
+    const { name, title, date, img, img_1, readingTime
+    } = props.blog;
     return (
-        <div>
-            <h1>Blog</h1>
+        <div className='my-12 px-8'>
+            <div >
+                <img className='rounded-lg' src={img} alt="" />
+                <div className=' flex mt-6'>
+                    <div className='basis-3/4 flex gap-4'>
+                        <img className="w-20 rounded-full" src={img_1} alt="" />
+                        <div>
+                            <h3 className='font-bold'>{name}</h3>
+                            <p className='text-slate-500'>{date}</p>
+                        </div>
+                    </div>
+                    <div className='basis-1/4 flex gap-3'>
+                        <p className='align-middle'>{readingTime}</p>
+                        <button className='btn'>
+                            <FontAwesomeIcon icon={faBookmark} />
+                        </button>
+                    </div>
+                </div>
+                <h2 className='text-3xl font-bold my-4'>{title}</h2>
+                <a className='link' href="">Mark as read</a>
+            </div>
         </div>
     );
 };
--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Bookmark = () => {
     return (
         <div>
-            <h1>Bookmark</h1>
+            <div className='mt-8  mb-6'>
+                <div className="border-2 border-indigo-600 bg-indigo-100 p-3 text-center rounded-md text-2xl font-bold">Spent time on read:{ } min</div>
+            </div>
+            <div className='px-6 py-4 bg-slate-200 rounded-md'>
+                <h2 className='text-2xl font-bold'>Bookmarked Blogs : { } </h2>
+                <div id='bookmarked-container' >
+
+                </div>
+            </div>
         </div>
     );
 };

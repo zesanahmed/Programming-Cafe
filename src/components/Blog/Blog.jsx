067cf71ce1b10c2ac7d2a498b40ff856c8cde2e8
zesanahmed <zesanahmed593@gmail.com>
2023-05-10 18:28:08 +0600

    blog component added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Blog = () => {
+const Blog = (props) => {
+    console.log(props)
     return (
         <div>
             <h1>Blog</h1>
--- src/components/Cafe/Cafe.jsx
+++ src/components/Cafe/Cafe.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Bookmark from '../Bookmark/Bookmark';
 import Blog from '../Blog/Blog';
 
+
 const Cafe = () => {
+
+    const [blogs, setBlog] = useState([]);
+
+    useEffect(() => {
+        fetch('info.json')
+            .then(res => res.json())
+            .then(data => setBlog(data))
+    }, []);
+
+
     return (
         <div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 m-8">
                 <div className="col-span-2">
-                    <Blog></Blog>
+                    {
+                        blogs.map(blog => <Blog
+                            key={blog.id}
+                            blog={blog}>
+                        </Blog>)
+                    }
                 </div>
                 <div className="col-span-1">
                     <Bookmark></Bookmark>
